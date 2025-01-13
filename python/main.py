81556279226a55ee9ccde284eed74c7e002a19fb
sricharan4402 <sricharan.n20@gmail.com>
2025-01-13 19:05:56 +0530

    Update main.py

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 async def connect_ais_stream():
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {"APIKey": "<YOUR API KEY>", "BoundingBoxes": [[[-11, 178], [30, 74]]]}
+        subscribe_message = {"APIKey": "d847a8793fa622d91be218450380214f69f2d888", "BoundingBoxes": [[[-11, 178], [30, 74]]]}
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
